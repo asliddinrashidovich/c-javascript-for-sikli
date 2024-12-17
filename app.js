ced8249c82e380a57593455afe7346d57466099e
Asliddin <asliddinrashidovich7@gmail.com>
2024-12-17 10:57:38 +0500

    masalalar 10 - gacha tugadi

---
+++ app.js
@@ -0,0 +1,127 @@
+// masala 1
+
+// let k = 5; // k soni
+// let n = 3; // n marta chiqarish (n > 0)
+
+// // k sonini n marta chiqarish
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+// maslala 2;
+
+// function maslala(a, b) {
+//     if(a < b) {
+//         for(let i = a; i <= b; i++) {
+//             console.log(i)
+//         }
+//         console.log(b-a+1 +' ta son bor')
+//     } else {
+//         console.log(`a soni b dan katta`)
+//     }
+// }
+// maslala(3,5)
+
+// masala 3;
+
+// function maslala(a, b) {
+//     if(a < b) {
+//         for(let i = (b-1); i > a; i--) {
+//             console.log(i)
+//         }
+//         console.log(b - a - 1 +' ta son bor')
+//     } else {
+//         console.log(`a soni b dan katta`)
+//     }
+// }
+// maslala(3,7)
+
+// masala 4;
+
+// function maslala(a) {
+//     for(let i = 1; i <= 10; i++) {
+//         console.log(a * i + ' som')
+//     }
+// }
+// maslala(25000);
+
+// masala 5;
+
+// function maslala(a) {
+//     for(let i = 1; i <= 10; i++) {
+//         console.log(a * i / 10 + ' som')
+//     }
+// }
+// maslala(25000)
+
+// masala 6;
+
+// function maslala(a) {
+//     for(let i = 12; i <= 20; i++) {
+//         if(i%2 == 0) {
+//             console.log(i / 10 * a + ' somdan')
+//         }
+//     }
+// }
+// maslala(25000)
+
+// masala 7;
+
+// function maslala(a, b) {
+//     if(a < b) {
+//         let num = 0;
+//         for(let i = a; i <= b; i++) {
+//             num += i;
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`a soni b dan katta`)
+//     }
+// }
+
+// maslala(1, 9)
+
+// masala 8;
+
+// function maslala(a, b) {
+//     if(a < b) {
+//         let num = 1;
+//         for(let i = a; i <= b; i++) {
+//             num *= i;
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`a soni b dan katta`)
+//     }
+// }
+// maslala(1,5)
+
+// masala 9;
+
+// function maslala(a, b) {
+//     if(a < b) {
+//         let num = 0;
+//         for(let i = a; i <= b; i++) {
+//             num += i**2;
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`a soni b dan katta`)
+//     }
+// }
+// maslala(1,3)
+
+// masala 10;
+
+// function maslala(n) {
+//     if(n > 0) {
+//         let num = 0;
+//         for(let i = 1; i <= n; i++) {
+//             num += (1 / i)
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`n soni 0 dan kichik` )
+//     }
+// }
+// maslala(0)
