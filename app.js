593fbd9f9ba9d9f3d455955a676797d83ee5966f
Asliddin <asliddinrashidovich7@gmail.com>
2024-12-18 10:11:17 +0500

    toplam 20 gacha tugadi

--- app.js
+++ app.js
@@ -125,3 +125,181 @@
 //     }
 // }
 // maslala(0)
+
+// masala 11
+
+
+// function masala(n) {
+//     let num = 0;
+//     if(n > 0) {
+//         for(let i = 0; i <= n; i++) {
+//             num += (n+i)**2;
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+
+// masala(2)
+
+// masala 12;
+
+// function masala(n) {
+//     let num = 1;
+//     if(n > 0) {
+//         for(let i = 11; i <= 10 + n; i++) {
+//             num *= i/ 10;
+//         }
+//         console.log(num)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+
+// masala(2)
+
+// maslaa 13
+
+// function masala(n) {
+//     let num = 0;
+//     let a = 1;
+//     if(n > 0) {
+//         for(let i = 11; i <= 10 + n; i++) {
+//             let term = i;
+//             num += (term * a);
+//             a *= -1
+//         }
+//         console.log(num / 10)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+
+// masala(3)
+
+// masala 14;
+
+// function masala(n) {
+//     let num = 0;
+//     let a = 1;
+//     if(n > 0) {
+//         for(let i = 1; i <= n; i++) {
+//             num += (2*i -1)
+//         }
+//         console.log(num )
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+
+// masala(3)
+
+// masala 15;
+
+// function masala(n, a) {
+//     let result = 1;
+//     if(n > 0) {
+//         for(let i = 0; i < n; i++) {
+//             result *= a
+//         }
+        
+//         console.log(result)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+
+// masala(3, 2)
+
+
+// masala 16;
+
+// function masala(n, a) {
+//     let result = 0;
+//     if(n > 0) {
+//         for(let i = 1; i <= n; i++) {
+//             result += (a ** i)
+//             console.log(result)
+//             result = 0
+//         }
+        
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+// masala(4, 2)
+
+// masala 17;
+
+// function masala(n, a) {
+//     let result = 0;
+//     let all = 0;
+//     if(n > 0) {
+//         for(let i = 0; i <= n; i++) {
+//             result += (a ** i)
+//             all += result
+//             console.log(result)
+//             result = 0;
+//         }
+//         console.log("Jami " + all)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+// masala(4, 2)
+
+// masala 18;
+
+// function masala(n, a) {
+//     let result = 0;
+//     let all = 0;
+//     let ish = 1
+//     if(n > 0) {
+//         for(let i = 0; i <= n; i++) {
+//             result += (a ** i)
+//             console.log(result);
+//             result *= ish;
+//             all += result
+//             ish *= -1
+//             result = 0;
+//         }
+//         console.log(`Jami yigi'indi ${all} ga teng`)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+// masala(3, 2)
+
+// masala 19;
+
+// function masala(n) {
+//     let result = 1;
+//     let all = 0;
+//     if(n > 0) {
+//         for(let i = 1; i <= n; i++) {
+//             result *= i
+//         }
+//         console.log(result)
+//     } else {
+//         console.log(`n soni manfiy son`)
+//     }
+// }
+// masala(5)
+
+// masala 20;
+
+function masala(n) {
+    let result = 1;
+    let all = 0;
+    if(n > 0) {
+        for(let i = 1; i <= n; i++) {
+            result *= i;
+            all += result
+        }
+        console.log(all)
+    } else {
+        console.log(`n soni manfiy son`)
+    }
+}
+masala(3)
